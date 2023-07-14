569181634e77c2a2d6332bd3cb0207ade57a2980
drufian1998 <82235017+drufian1998@users.noreply.github.com>
2023-07-13 23:00:48 -0400

    Update crear_usuario.php

--- crear_usuario.php
+++ crear_usuario.php
@@ -5,15 +5,15 @@ $username = "root";
 $password = "";
 $dbname = "usuarios";
 
-// Crear conexión
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar la conexión
+
 if ($conn->connect_error) {
     die("Error en la conexión a la base de datos: " . $conn->connect_error);
 }
 
-// Crear un nuevo usuario
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST["nombre"];
     $email = $_POST["email"];
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sql = "INSERT INTO usuarios_table (nombre, email, password) VALUES ('$nombre', '$email', '$password')";
 
     if ($conn->query($sql) === TRUE) {
-        header("Location: index.html"); // Redireccionar a la página principal
+        header("Location: index.html");
         exit();
     } else {
         echo "Error al crear el usuario: " . $conn->error;
